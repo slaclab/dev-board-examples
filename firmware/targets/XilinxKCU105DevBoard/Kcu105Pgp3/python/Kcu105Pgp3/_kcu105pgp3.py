42ea2ab9c4285794ac44d44aa6ebc3c77fac5fd5
Benjamin Reese <bareese@slac.stanford.edu>
2017-09-27 16:15:14 -0700

    Rogue devices for PGP3 test

--- firmware/targets/XilinxKCU105DevBoard/Kcu105Pgp3/python/Kcu105Pgp3/_kcu105pgp3.py
+++ firmware/targets/XilinxKCU105DevBoard/Kcu105Pgp3/python/Kcu105Pgp3/_kcu105pgp3.py
@@ -19,17 +19,62 @@
 #-----------------------------------------------------------------------------
 
 import pyrogue as pr
+import pyrogue.simulation
+import pyrogue.gui 
 
 import surf.axi
 import surf.protocols.ssi
+import surf.protocols.pgp
 
-class Pgp3Vcs(pr.Device):
+import logging
+import PyQt4.QtGui
+import PyQt4.QtCore
+import sys
+
+
+class Kcu105Pgp3(pr.Device):
     def __init__(self, channels=4, **kwargs):
                  
-        super().__init__(**kwargs):
+        super().__init__(**kwargs)
+
+        self.add(surf.protocols.pgp.Pgp3AxiL(
+            offset = 0x10000000 + 0x10000*(2*channels),
+            channels = channels,
+            writeEn = False,
+            errorCountBits = 4,
+            statusCountBits = 32))        
 
         for i in range(0, channels*2, 2):
-            self.add(surf.protocols.ssi.SsiPrbsTx(name=f'Ch{i}PrbsTx', offset=0x00010000*i))
-            self.add(surf.protocols.ssi.SsiPrbsRx(name=f'Ch{i}PrbsRx', offset=0x00020000*i))        
+            self.add(surf.protocols.ssi.SsiPrbsTx(name=f'Ch{i}PrbsTx', offset=0x10010000*i))
+            self.add(surf.protocols.ssi.SsiPrbsRx(name=f'Ch{i}PrbsRx', offset=0x10020000*i))        
 
-     
+
+
+
+class Kcu105Pgp3Root(pr.Root):
+    def __init__(self, **kwargs):
+        super().__init__(name='Kcu105', description='', **kwargs)
+
+        #vc = rogue.hardware.pgp.PgpCard('/dev/pgpcard_0', 0, 0)
+        #srp = rogue.protocols.srp.SrpV3()
+        #pr.streamConnectBiDir(vc, srp)
+        srp = pyrogue.simulation.MemEmulate()
+        
+
+        logging.getLogger('pyrogue.SRP').setLevel(logging.DEBUG)
+        
+        self.add(Kcu105Pgp3(memBase=srp))
+
+        self.start(pollEn=False)
+
+        
+if __name__ == '__main__':
+
+    with Kcu105Pgp3Root() as root:
+        appTop = PyQt4.QtGui.QApplication(sys.argv)
+        guiTop = pyrogue.gui.GuiTop(group='Pgp3')
+        guiTop.addTree(root)
+        guiTop.resize(1000,1000)
+
+        # Run gui
+        appTop.exec_()
