30a5be253351d6947f72564bbc8e8f55376f472c
Larry Ruckman <ruckman@slac.stanford.edu>
2020-06-18 12:21:11 -0700

    updating submoduels

--- firmware/targets/XilinxKCU105DevBoard/Kcu105FifoAsync/ruckus.tcl
+++ firmware/targets/XilinxKCU105DevBoard/Kcu105FifoAsync/ruckus.tcl
@@ -2,7 +2,7 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load common and sub-module ruckus.tcl files
-loadRuckusTcl $::env(PROJ_DIR)/../../../
+loadRuckusTcl $::env(PROJ_DIR)/../../../submodules/surf
 
 # Load local source Code and constraints
 loadSource      -dir "$::DIR_PATH/hdl"
