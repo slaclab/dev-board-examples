ba8e87191480e39505dbbc3a25ac6429a4c0b322
Larry Ruckman <ruckman@slac.stanford.edu>
2020-11-19 13:03:36 -0800

    SW clean up

--- software/scripts/DevBoardGui.py
+++ software/scripts/DevBoardGui.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 #-----------------------------------------------------------------------------
 # This file is part of the 'Development Board Examples'. It is subject to
 # the license terms in the LICENSE.txt file found in the top-level directory
@@ -29,96 +30,96 @@ import DevBoard as devBoard
 
 #################################################################
 
-# Convert str to bool
-argBool = lambda s: s.lower() in ['true', 't', 'yes', '1']
+if __name__ == "__main__":
 
-# Set the argument parser
-parser = argparse.ArgumentParser()
+    # Convert str to bool
+    argBool = lambda s: s.lower() in ['true', 't', 'yes', '1']
 
-# Add arguments
-parser.add_argument(
-    "--type",
-    type     = str,
-    required = True,
-    help     = "define the type of interface",
-)
+    # Set the argument parser
+    parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    "--dev",
-    type     = str,
-    required = False,
-    default  = '/dev/datadev_0',
-    help     = "true to show gui",
-)
+    # Add arguments
+    parser.add_argument(
+        "--type",
+        type     = str,
+        required = True,
+        help     = "define the type of interface",
+    )
 
-parser.add_argument(
-    "--ip",
-    type     = str,
-    required = False,
-    default  = '192.168.2.10',
-    help     = "IP address",
-)
+    parser.add_argument(
+        "--dev",
+        type     = str,
+        required = False,
+        default  = '/dev/datadev_0',
+        help     = "true to show gui",
+    )
 
-parser.add_argument(
-    "--lane",
-    type     = int,
-    required = False,
-    default  = 0,
-    help     = "PGP Lane",
-)
+    parser.add_argument(
+        "--ip",
+        type     = str,
+        required = False,
+        default  = '192.168.2.10',
+        help     = "IP address",
+    )
 
-parser.add_argument(
-    "--fpgaType",
-    type     = str,
-    required = False,
-    default  = '',
-    help     = "fpgaType = [empty_string,7series,ultrascale]",
-)
+    parser.add_argument(
+        "--lane",
+        type     = int,
+        required = False,
+        default  = 0,
+        help     = "PGP Lane",
+    )
 
-parser.add_argument(
-    "--pollEn",
-    type     = argBool,
-    required = False,
-    default  = True,
-    help     = "auto-polling",
-)
+    parser.add_argument(
+        "--fpgaType",
+        type     = str,
+        required = False,
+        default  = '',
+        help     = "fpgaType = [empty_string,7series,ultrascale]",
+    )
 
-parser.add_argument(
-    "--initRead",
-    type     = argBool,
-    required = False,
-    default  = True,
-    help     = "Enable read all variables at start",
-)
+    parser.add_argument(
+        "--pollEn",
+        type     = argBool,
+        required = False,
+        default  = True,
+        help     = "auto-polling",
+    )
 
-parser.add_argument(
-    "--enPrbs",
-    type     = argBool,
-    required = False,
-    default  = True,
-    help     = "Enable PRBS testing",
-)
+    parser.add_argument(
+        "--initRead",
+        type     = argBool,
+        required = False,
+        default  = True,
+        help     = "Enable read all variables at start",
+    )
 
-parser.add_argument('--html', help='Use html for tables', action="store_true")
+    parser.add_argument(
+        "--enPrbs",
+        type     = argBool,
+        required = False,
+        default  = True,
+        help     = "Enable PRBS testing",
+    )
 
-# Get the arguments
-args = parser.parse_args()
+    parser.add_argument('--html', help='Use html for tables', action="store_true")
 
-#################################################################
+    # Get the arguments
+    args = parser.parse_args()
 
-with devBoard.MyRoot(
-        name        = 'System',
-        description = 'Front End Board',
-        type        = args.type,
-        dev         = args.dev,
-        ip          = args.ip,
-        lane        = args.lane,
-        enPrbs      = args.enPrbs,
-        fpgaType    = args.fpgaType,
-        pollEn      = args.pollEn,
-        initRead    = args.initRead,
-    ) as root:
+    #################################################################
 
-    pyrogue.pydm.runPyDM(root=root)
+    with devBoard.MyRoot(
+            type        = args.type,
+            dev         = args.dev,
+            ip          = args.ip,
+            lane        = args.lane,
+            enPrbs      = args.enPrbs,
+            fpgaType    = args.fpgaType,
+            pollEn      = args.pollEn,
+            initRead    = args.initRead,
+        ) as root:
 
-#################################################################
+        pyrogue.pydm.runPyDM(root=root)
+
+    #################################################################
